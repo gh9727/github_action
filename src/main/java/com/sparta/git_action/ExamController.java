f4eb2c4540fd513f221928cd5c043838f86eedb2
gh9727 <gj9727@gmail.com>
2026-03-04 15:03:14 +0900

    github_action 실행 되는지 확인해보기 위한 develop 브랜치 푸쉬

--- src/main/java/com/sparta/git_action/ExamController.java
+++ src/main/java/com/sparta/git_action/ExamController.java
@@ -10,4 +10,9 @@ public class ExamController {
         return "도커파일을 빌드해서 도커 이미지 생성 후 생성된 이미지를 도커 허브에 올리고" +
                 "aws에 도커 설정해서 내가 도커 허브에 올린 도커 이미지를 받아와 실행시킨다";
     }
+
+    @GetMapping("/git/action")
+    public String getGitAction() {
+        return "gitHub_Action을 사용해서 자동 배포를 성공했다!";
+    }
 }
